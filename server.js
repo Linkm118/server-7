da1b2342851810e78553ade7ba6a60f7be4bd280
Linkm118 <206734997+Linkm118@users.noreply.github.com>
2025-11-14 12:04:48 +0900

    リソース作成

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const kv = await Deno.openKv();
 
 /***  リソースの作成 ***/
 app.post('/api/pokemons', async (c) => {
-  return c.json({ path: c.req.path });
+  const body = await c.req.parseBody();
+  const record = JSON.parse(body.record);
+
+  const id = await getNextId();
+  record.id = id;
+  record.createdAt = new Date().toISOString();
+
+  return c.json({ record });
 });
 
 /*** リソースの取得（レコード単体） ***/
